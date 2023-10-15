a86d8abd02e9300bf943b37b22882f24258ff6e6
Jhonathan Oliveira <jhoneoliveiratetris@gmail.com>
2023-10-15 12:32:31 -0300

    Projeto de ordenação de arquivos utilizando o método de ordenação MergeSort

---
+++ QuartoPeriodo/Estrutura_de_Dados II/ArrayAtividade09EDII/vetoraleatorio.h
@@ -0,0 +1,45 @@
+#ifndef VETORALEATORIO_H
+#define VETORALEATORIO_H
+#include <QString>
+#include <ctime>
+#include <random>
+#include <bits/stdc++.h>
+
+namespace jhone{
+class VetorAleatorio
+{
+private:
+    int tamanho;
+    int *array;
+    int *arraySelectionSort;
+    int *arrayInsertionSort;
+    int *arrayBubbleSort;
+    int *arrayMergeSort;
+    int numExec;
+    int numOrdenSelectionSort;
+    int numOrdenInsertionSort;
+    int numOrdenBubbleSort;
+public:
+    VetorAleatorio(int tamanho);
+    ~VetorAleatorio();
+    int getTamanho() const;
+    QString getConjunto();
+    QString getConjuntoOrdenadoSelectionSort();
+    QString getConjuntoOrdenadoInsertionSort();
+    QString getConjuntoOrdenadoBubbleSort();
+    QString getConjuntoOrdenadoMergeSort();
+    int buscarNumero(int aux);
+    int buscarNumeroMelhorado(int aux);
+    int buscarBinaria(int aux);
+    void selectionSort();
+    void insertionSort();
+    void bubbleSort();
+    void merge(int arr[], int p, int q, int r);
+    void mergeSort(int arr[], int l, int r);
+    int getNumExec() const;
+    int getNumOrdenSelectionSort() const;
+    int getNumOrdenInsertionSort() const;
+    int getNumOrdenBubbleSort() const;
+};
+}
+#endif // VETORALEATORIO_H
